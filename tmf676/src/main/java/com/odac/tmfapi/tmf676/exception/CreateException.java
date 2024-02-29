43c3a0fd97d534679947ab6ed6fd88763b18fd6f
Gireesh Kumar <gk.malattiri@gmail.com>
2024-02-29 22:37:28 +0400

    Refactoring and pagination

---
+++ tmf676/src/main/java/com/odac/tmfapi/tmf676/exception/CreateException.java
@@ -0,0 +1,8 @@
+package com.odac.tmfapi.tmf676.exception;
+
+
+public class CreateException extends RuntimeException {
+    public CreateException(String message) {
+        super(message);
+    }
+}
